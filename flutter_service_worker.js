1cfa2458d27b0f58535b784fe78a43a1638d43ac
Vasilis M. <32592902+arg0nath@users.noreply.github.com>
2024-11-24 22:42:15 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c732423a66a1cf73e1ff23b4f003336b
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "4b4e181295869ac800358dfaafe076b2",
+"flutter_bootstrap.js": "612f56e0c9ee3f1f671d93d0241481d5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
